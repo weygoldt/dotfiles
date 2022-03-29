4fabe1372498504478f0a8f55f2121f03563cca3
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-29 14:17:12 +0200

    update 2022/03/29, 14:17

---
+++ Scripts/md2pdf.sh
@@ -0,0 +1,11 @@
+
+
+pandoc "$1" \
+    -f gfm \
+    -V linkcolor:blue \
+    -V geometry:a4paper \
+    -V geometry:margin=2.5cm \
+    -V mainfont="Segoe UI" \
+    -V monofont="Source Code Pro" \
+    --pdf-engine=xelatex \
+    -o "$2"
